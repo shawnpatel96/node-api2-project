3f617ffefde51149af1828a4699ea22c1eedb262
shawn <shawnpatel196@gmail.com>
2020-03-12 19:12:05 -0500

    deploy

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
-const environment = process.env.DB_ENV || 'development';
-const knexConfig = require('../knexfile.js')[environment];
+// const environment = process.env.DB_ENV || 'development';
+const knexConfig = require('../knexfile.js')
 const db = knex(knexConfig.development);
 
 module.exports = {
